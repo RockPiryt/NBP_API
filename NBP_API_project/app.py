058706235f9d78aa3e69c19618e9d1c36358e6e9
Paulina Kimak <rockpiryt@gmail.com>
2024-03-15 17:53:38 +0100

    'RockPiryt' - add formatted datetime

--- NBP_API_project/app.py
+++ NBP_API_project/app.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 
 # #Get response from API
 # response = requests.get(url="http://api.nbp.pl/api/cenyzlota/today")
@@ -40,3 +41,9 @@ currency_euro = response.json()[0]["rates"][7]["currency"]
 mid_euro = response.json()[0]["rates"][7]["mid"]
 print(currency_euro)
 print(mid_euro)
+
+#____________________Given a date (formatted YYYY-MM-DD)
+user_year = 2021
+user_month = 7
+user_day = 22
+user_date = datetime(year=user_year, month=user_month, day=user_day).strftime("%Y-%m-%d")
