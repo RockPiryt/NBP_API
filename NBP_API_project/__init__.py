38085e4431f36400f485dc84d7e8744aff14d48a
PaulinaKimak <rockpiryt@gmail.com>
2024-03-17 12:02:33 +0100

    'RockPiryt' - correct endpoints

--- NBP_API_project/__init__.py
+++ NBP_API_project/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from configuration import Configuration
 
-from NBP_API_project import operations
 
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
 
+from NBP_API_project import operations
 
 
 # @app.route('/')
--- NBP_API_project/operations.py
+++ NBP_API_project/operations.py
@@ -10,7 +10,7 @@ def operation1():
         'data': 'Get first info'
     }), 200
 
-@app.route('/api/v1/operation2/<str:curr_code>/<int:last_quo>', methods=['GET'])
+@app.route('/api/v1/operation2/<curr_code>/<int:last_quo>', methods=['GET'])
 def operation2(curr_code, last_quo):
     """Given a currency code and the number of last quotations N (N <= 255), provide the max and min average value (every day has a different average)."""
 
@@ -19,7 +19,7 @@ def operation2(curr_code, last_quo):
         'data': f'Get second info with {curr_code} and {last_quo}'
     }), 200
 
-@app.route('/api/v1/operation3/<str:curr_code>/<int:last_quo>', methods=['GET'])
+@app.route('/api/v1/operation3/<curr_code>/<int:last_quo>', methods=['GET'])
 def operation3(curr_code, last_quo):
     """Given a currency code and the number of last quotations N (N <= 255), provide the max and min average value (every day has a different average)."""
 
