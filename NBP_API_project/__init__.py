388e33fbc52f8ee8106c380b0c955b8c1b744bdb
PaulinaKimak <rockpiryt@gmail.com>
2024-03-18 09:41:26 +0100

    'RockPiryt' - add DB configuration

--- NBP_API_project/__init__.py
+++ NBP_API_project/__init__.py
@@ -1,14 +1,46 @@
+# import os
+# from os.path import join, dirname
+# from dotenv import load_dotenv
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 from configuration import Configuration
 
 
+#___________________________________________________________________________
+# # Load .env file
+# dotenv_path = join(dirname(__file__), '.env')
+# load_dotenv(dotenv_path)
+
 
 app = Flask(__name__)
+
+#Configuration from FILE configuration.py
 app.config.from_object(Configuration)
 
 from NBP_API_project import operations
 
+# SQLAlchemy (create Python object from tables records)
+db = SQLAlchemy(app)
+app.app_context().push()
 
-# @app.route('/')
-# def index():
-#     return 'Hello I am Paulina'
+Migrate(app,db)
+
+
+
+
+
+
+
+# # Configuration here (not recomended)
+# # ----------------------------------- DB SQLite
+# basedir = os.path.abspath(os.path.dirname(__file__))
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'data.sqlite')
+
+
+# # -----------------------------------DB PostgreSQL
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLalchemy_database_uri")
+
+
+
--- configuration.py
+++ configuration.py
@@ -9,6 +9,11 @@ load_dotenv(dotenv_path)
 class Configuration:
     DEBUG = True
     SECRET_KEY = os.getenv("flask_secret_key")
+    SQLALCHEMY_DATABASE_URI = os.getenv("Postgres_db_uri")
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLite_db_uri")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
 
 
 # To check correct loading
