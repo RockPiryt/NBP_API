de0405248e4ebca828af0a7d617831903dcb09ad
PaulinaKimak <rockpiryt@gmail.com>
2024-03-25 22:35:18 +0100

    'RockPiryt' - add markers

--- tests/py_assertions/test_module01.py
+++ tests/py_assertions/test_module01.py
@@ -1,26 +1,33 @@
 import pytest
+pytestmark = [pytest.mark.mod1, pytest.mark.mod2]
 
 
+@pytest.mark.pinia
 def test_a1():
     assert 4 > 3
 
 
 def test_a2():
-    assert True # passed
-    assert 1 # passed
-    assert 0 # failed
-    assert False # failed
+    assert True  # passed
+    assert 1  # passed
+    assert 0  # failed
+    assert False  # failed
 
 
+@pytest.mark.pinia
+@pytest.mark.stringtest
 def test_a3():
     assert "abcde" == "abcde"
 
+
+@pytest.mark.pinia
 def test_a4():
     assert ((3-1)*4/2) == 4.0
 
+
 def test_a5():
-    assert 1 in divmod(9,5)
+    assert 1 in divmod(9, 5)
     assert 'py' in 'python'
-    assert 2 in [1,2,4] # passed
-    assert [1,2] in [1,2,4] # failed
-    assert [1,2] in [[1,2],[4,0]] # passed (sublist in list of list)
+    assert 2 in [1, 2, 4]  # passed
+    assert [1, 2] in [1, 2, 4]  # failed
+    assert [1, 2] in [[1, 2], [4, 0]]  # passed (sublist in list of list)
