bae4a92c8010f5ca629d5f94637271cafed77165
PaulinaKimak <rockpiryt@gmail.com>
2024-03-28 11:54:31 +0100

    'RockPiryt' - add test: test_get_av_exchange_rates_all

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -8,3 +8,13 @@ def test_get_av_exchange_rates_no_records(client_setup):
 
     assert response.status_code == 200
     assert response.get_json() == expected_results
+
+
+def test_get_av_exchange_rates_all(client_setup):
+    response = client_setup.get('/api/v1/av_exchange_rates')
+    response_data = response.get_json()
+
+
+    assert response.status_code == 200
+    assert response_data['success'] is True
+    assert response_data['number_of_records'] == 9
